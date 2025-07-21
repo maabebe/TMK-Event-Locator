cf26fe682d87870f63f45a79656fdca08efba6bf
maabebe <mabeb@illinois.edu>
2025-07-20 20:06:29 -0500

    Update EventModel.js
    This code defines a class called Event that creates event objects with properties like name, location, date, language, instructor, and type, and exports it for use in other files.

--- backend/models/EventModel.js
+++ backend/models/EventModel.js
@@ -1,1 +1,13 @@
+class Event {
+  constructor({ event_name, location_zip, date, language, instructor, type }) {
+    this.event_name = event_name;
+    this.location_zip = location_zip;
+    this.date = new Date(date); // Store as Date object
+    this.language = language;
+    this.instructor = instructor;
+    this.type = type;
+  }
+}
+
+module.exports = Event;
 
