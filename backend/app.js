3525de0b52734661b3de15b843472e68760ebd9c
maabebe <mabeb@illinois.edu>
2025-07-20 20:10:26 -0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const { sampleEvents } = require('./events');
+const app = express();
+
+app.get('/events', (req, res) => {
+  res.json(sampleEvents);
+});
+
+module.exports = app;
 
